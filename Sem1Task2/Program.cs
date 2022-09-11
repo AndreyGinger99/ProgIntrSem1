d9cedc336c48651f80ad6ddf7e587593ff5ee36a
Andrew <andreys71.ru@gmail.com>
2022-09-11 21:52:46 +0300

    2 и 4 задание сделаны

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿///==============================================
+// # 2 Напишите программу, которая на вход принимает два числа
+// и выдает какое больше, какое меньше
+// ==============================================
+string? inputLineA = Console.ReadLine();    //Вводим числа
+string? inputLineB = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null) //Проверка на не НУЛЛ
+{
+
+    int inputNumberA = int.Parse(inputLineA); // Переводим в integer
+    int inputNumberB = int.Parse(inputLineB);
+
+    //int outNumber = 0;
+    if (inputNumberA > inputNumberB) // Проверяем больше ли число А чем число Б
+    {
+        Console.WriteLine("Число "); // Выводим если больше
+        Console.WriteLine(inputNumberA);
+        Console.WriteLine(" больше чем  ");
+        Console.WriteLine(inputNumberB);
+    }
+    else 
+    {
+        Console.WriteLine("Число "); // Выводим если меньше
+        Console.WriteLine(inputNumberB);
+        Console.WriteLine(" больше чем  ");
+        Console.WriteLine(inputNumberA);
+    }
+
+    
+}
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿///==============================================
+// # 4  Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+// ==============================================
+
+
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+string? inputLineC = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null && inputLineC != null) //Проверка на не НУЛЛ
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+    int inputNumberC = int.Parse(inputLineC);
+    int maxNum = inputNumberA;
+    if (inputNumberB > maxNum)
+    { 
+        maxNum = inputNumberB;
+        if (inputNumberC > maxNum)
+        { 
+        maxNum = inputNumberC;
+        }
+    }
+    else 
+    if (inputNumberC > maxNum)
+    { 
+        maxNum = inputNumberC;
+    }
+    Console.WriteLine("Число ");
+    Console.WriteLine(maxNum);
+    Console.WriteLine(" наибольшее");
+
+
+}
+   

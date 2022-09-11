4a9b7f355360f71d75998687254318d7798223fe
Andrew <andreys71.ru@gmail.com>
2022-09-11 21:24:17 +0300

    Добавлены все папки

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,21 @@
+﻿///==============================================
+// # 0 Напишите программу, которая на входе принимает число
+// и выдет его квадрат
+// ==============================================
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    //int outNumber = 0;
+    DateTime d1 = DateTime.Now;
+    for (int i = 0; i < 10000000; i++)
+    {
+        int outNumber = inputNumber * inputNumber;
+        //int outNumber = (int)Math.Pow(inputNumber,2);
+    }
+    DateTime d2 = DateTime.Now;
+    Console.WriteLine(d2-d1);
+    Console.Write("Вот время начала");
+    Console.WriteLine(d1);
+    //Console.WriteLine(outNumber);
+}
---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿///==============================================
+// # 2 Напишите программу, которая на вход принимает два числа
+// и проверяет является ли первое число квадратом второго
+// ==============================================
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+    //int outNumber = 0;
+
+    //bool outResult = (inputNumberA * inputNumberA == inputNumberB);
+    bool outResult = (Math.Sqrt(inputNumberB) == inputNumberA);
+
+    //int outNumber = inputNumber * inputNumber;
+    //int outNumber = (int)Math.Pow(inputNumber,2);
+
+    Console.WriteLine(outResult);
+}
